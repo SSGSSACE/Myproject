a32165bda57fa2a663b400df3baa6116631e48b7
SSGSSACE <quangbayhai@gmail.com>
2022-11-04 14:11:13 +0000

    Nov 4, 2022, 9:11 PM

--- build/gui.py
+++ build/gui.py
@@ -13,7 +13,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"E:\Python\Myproject\build\assets\frame0")
 
-
+#This is my comment
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
